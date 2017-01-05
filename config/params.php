6760778f39247897cfefb96bce51247107ce6967
Dmitriy Gorbatko <kefir266@yahoo.com>
2017-01-06 00:52:04 +0300

    Init project

---
+++ config/params.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'adminEmail' => 'kefir266@yahoo.com',
+];
---
+++ views/site/about.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        This is test assignment to junior PHP developer position.
+    </p>
+    <a href="https://github.com/kefir266/testmeteo">GitHub</a>
+
+
+</div>
